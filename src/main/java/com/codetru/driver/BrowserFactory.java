d6194a1f4394839c6302057c0bdce89985b4d902
fasiash <fasiuddin@codetru.com>
2024-10-28 14:51:34 +0530

    verifying remote jenkins

--- src/main/java/com/codetru/driver/BrowserFactory.java
+++ src/main/java/com/codetru/driver/BrowserFactory.java
@@ -5,7 +5,6 @@
 
 package com.codetru.driver;
 
-import com.codetru.constants.FrameworkConstants;
 import com.codetru.exceptions.HeadlessNotSupportedException;
 
 import org.openqa.selenium.MutableCapabilities;
@@ -22,21 +21,18 @@ import org.openqa.selenium.safari.SafariOptions;
 import java.util.HashMap;
 import java.util.Map;
 
-import static java.lang.Boolean.TRUE;
-
 public enum BrowserFactory {
 
     CHROME {
         @Override
         public WebDriver createDriver() {
-
             return new ChromeDriver(getOptions());
         }
 
         @Override
         public ChromeOptions getOptions() {
             ChromeOptions options = new ChromeOptions();
-            Map<String, Object> prefs = new HashMap<String, Object>();
+            Map<String, Object> prefs = new HashMap<>();
             prefs.put("profile.default_content_setting_values.notifications", 2);
             prefs.put("credentials_enable_service", false);
             prefs.put("profile.password_manager_enabled", false);
@@ -47,48 +43,39 @@ public enum BrowserFactory {
             options.addArguments("--disable-infobars");
             options.addArguments("--disable-notifications");
             options.addArguments("--remote-allow-origins=*");
-            options.addArguments("--headless");
-            options.addArguments("window-size=1800,900");
+            options.addArguments("--headless"); // Enable headless mode
+            options.addArguments("window-size=1800,900"); // Set the window size
             options.setAcceptInsecureCerts(true);
-
-//            if (Boolean.valueOf(FrameworkConstants.HEADLESS) == true) {
-//                options.addArguments("--headless=new");
-//                options.addArguments("window-size=1800,900");
-//            }
 
             return options;
         }
-    }, FIREFOX {
+    },
+    FIREFOX {
         @Override
         public WebDriver createDriver() {
-
             return new FirefoxDriver(getOptions());
         }
 
         @Override
         public FirefoxOptions getOptions() {
             FirefoxOptions options = new FirefoxOptions();
-
             options.setAcceptInsecureCerts(true);
-
-//            if (Boolean.valueOf(FrameworkConstants.HEADLESS) == true) {
-//                options.addArguments("--headless");
-//                options.addArguments("window-size=1800,900");
-//            }
+            options.addArguments("--headless"); // Enable headless mode
+            options.addArguments("window-size=1800,900"); // Set the window size
 
             return options;
         }
-    }, EDGE {
+    },
+    EDGE {
         @Override
         public WebDriver createDriver() {
-
             return new EdgeDriver(getOptions());
         }
 
         @Override
         public EdgeOptions getOptions() {
             EdgeOptions options = new EdgeOptions();
-            Map<String, Object> prefs = new HashMap<String, Object>();
+            Map<String, Object> prefs = new HashMap<>();
             prefs.put("profile.default_content_setting_values.notifications", 2);
             prefs.put("credentials_enable_service", false);
             prefs.put("profile.password_manager_enabled", false);
@@ -99,20 +86,16 @@ public enum BrowserFactory {
             options.addArguments("--disable-infobars");
             options.addArguments("--disable-notifications");
             options.addArguments("--remote-allow-origins=*");
-
+            options.addArguments("--headless"); // Enable headless mode
+            options.addArguments("window-size=1800,900"); // Set the window size
             options.setAcceptInsecureCerts(true);
-
-//            if (Boolean.valueOf(FrameworkConstants.HEADLESS) == true) {
-//                options.addArguments("--headless=new");
-//                options.addArguments("window-size=1800,900");
-//            }
 
             return options;
         }
-    }, SAFARI {
+    },
+    SAFARI {
         @Override
         public WebDriver createDriver() {
-
             return new SafariDriver(getOptions());
         }
 
@@ -121,14 +104,12 @@ public enum BrowserFactory {
             SafariOptions options = new SafariOptions();
             options.setAutomaticInspection(false);
 
-//            if (TRUE.equals(Boolean.valueOf(FrameworkConstants.HEADLESS)))
-//                throw new HeadlessNotSupportedException(options.getBrowserName());
+            // Safari does not support headless mode, so you can choose to throw an exception or leave it as is
+            throw new HeadlessNotSupportedException(options.getBrowserName());
 
-            return options;
+            // Alternatively, you could implement a non-headless approach for Safari
         }
     };
-
-    private static final String START_MAXIMIZED = "--start-maximized";
 
     public abstract WebDriver createDriver();
 

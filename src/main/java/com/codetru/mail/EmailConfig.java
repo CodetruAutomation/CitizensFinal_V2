2026f4b1253c47e7077b9ddf3565688c91968d1b
fasiash <fasiuddin@codetru.com>
2024-10-29 09:16:30 +0530

    added email code

--- src/main/java/com/codetru/mail/EmailConfig.java
+++ src/main/java/com/codetru/mail/EmailConfig.java
@@ -19,11 +19,11 @@ public class EmailConfig {
     //send_email_to_users=yes
 
     public static final String SERVER = "smtp.gmail.com";
-    public static final String PORT = "587";
+    public static final String PORT = "465";
 
     public static final String FROM = "codetruteamlead@gmail.com";
-    public static final String PASSWORD = "siye reij yhec cgwo";
+    public static final String PASSWORD = "verc heik ibom uhig";
 
-    public static final String[] TO = {"codetrutest@gmail.com"};
+    public static final String[] TO = {"fasiuddin@codetru.com"};
     public static final String SUBJECT = REPORT_TITLE;
 }
